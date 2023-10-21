f3b1b1b36cf7a2a54dd3a574b3aef397450f6a50
thomascard2004 <thomascard.2004@gmail.com>
2023-10-21 16:12:12 -0300

    Create mul_matrizes.py

---
+++ curiosidades/mul_matrizes.py
@@ -0,0 +1,20 @@
+def multiplicar_Matrizes(A,B):
+
+    num_linhasA, num_colunasA = len(A), len(A[0])
+    num_linhasB, num_colunasB = len(B), len(B[0])
+
+    matrizresultado = []
+
+    if (num_colunasA != num_linhasB):
+        print("As matrizes não podem ser multiplicadas")
+
+    else:
+        matrizresultado = [[0]*num_colunasB for _ in range(num_linhasA)]
+
+        for i in range(num_linhasA):
+            for j in range(num_colunasB):
+                for k in range(num_linhasB):
+                    matrizresultado[i][j] += A[i][k] * B[k][j]
+    
+    
+    return matrizresultado                
